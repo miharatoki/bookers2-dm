3da1d754070ce22d5363a2fc930ab7b4f4cc95d5
miharatoki <kou56653@gmail.com>
2021-07-25 10:06:48 +0000

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  root 'homes#top'
+  devise_for :users
+  get 'home/about' => 'homes#about'
+  resources :users,only: [:show,:index,:edit,:update]
+  resources :books
+end
