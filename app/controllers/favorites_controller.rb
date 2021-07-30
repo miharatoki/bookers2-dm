9731a54979f7e5a2d034967e848646ed6126a7b7
miharatoki <kou566533@gmail.com>
2021-07-30 06:27:19 +0000

    いいね機能非同期化

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,19 +1,17 @@
 class FavoritesController < ApplicationController
   def create
-    book = Book.find(params[:book_id])
-    favorite = current_user.favorites.new(book_id:book.id)
+    @book = Book.find(params[:book_id])
+    favorite = current_user.favorites.new(book_id:@book.id)
     begin
      favorite.save!
-     redirect_to request.referer
     rescue ActiveRecord::RecordInvalid => e
      puts e
     end
   end
 
   def destroy
-    book = Book.find(params[:book_id])
-    favorite = current_user.favorites.find_by(book_id:book.id)
+    @book = Book.find(params[:book_id])
+    favorite = current_user.favorites.find_by(book_id:@book.id)
     favorite.destroy
-    redirect_to request.referer
   end
 end
